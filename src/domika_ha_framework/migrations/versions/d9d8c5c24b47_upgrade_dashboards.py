d13f2f5d6284e5abd888fd33388d4ffaa5390e79
Artem Bezborodko <artem.bezborodko@atisgroup.ru>
2024-09-10 15:56:41 +0300

    Fixed migration issues

--- src/domika_ha_framework/migrations/versions/d9d8c5c24b47_upgrade_dashboards.py
+++ src/domika_ha_framework/migrations/versions/d9d8c5c24b47_upgrade_dashboards.py
@@ -12,24 +12,24 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = 'd9d8c5c24b47'
-down_revision: Union[str, None] = '9291d34e8062'
+revision: str = "d9d8c5c24b47"
+down_revision: Union[str, None] = "9291d34e8062"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     """Upgrade step."""
-    with op.batch_alter_table('dashboards') as batch_op:
-        batch_op.alter_column('dashboard', new_column_name='dashboards')
-        batch_op.add_column(sa.Column('hash', sa.String(), nullable=False, server_default=''))
+    with op.batch_alter_table("dashboards") as batch_op:
+        batch_op.alter_column("dashboard", new_column_name="dashboards")
+        batch_op.add_column(sa.Column("hash", sa.String(), nullable=False, server_default=""))
 
-    with op.batch_alter_table('dashboards') as batch_op:
-        batch_op.alter_column('hash', server_default=None)
+    with op.batch_alter_table("dashboards") as batch_op:
+        batch_op.alter_column("hash", server_default=None)
 
 
 def downgrade() -> None:
     """Downgrade step."""
-    with op.batch_alter_table('dashboards') as batch_op:
-        batch_op.alter_column('dashboards', new_column_name='dashboard')
-        batch_op.drop_column('hash')
+    with op.batch_alter_table("dashboards") as batch_op:
+        batch_op.alter_column("dashboards", new_column_name="dashboard")
+        batch_op.drop_column("hash")
