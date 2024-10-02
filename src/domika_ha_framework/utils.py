0d97c0f8017ba5ac7b5e1e5618b73b02bd73fdbe
Artem Bezborodko <artem.bezborodko@atisgroup.ru>
2024-10-02 12:58:51 +0300

    Added chunks function to the utils

--- src/domika_ha_framework/utils.py
+++ src/domika_ha_framework/utils.py
@@ -10,20 +10,23 @@ Author(s): Artem Bezborodko
 
 import datetime
 import enum
+import itertools
 from collections.abc import Iterable, Mapping
 from pathlib import Path
-from typing import Any
+from typing import Any, Iterator, TypeVar
+
+T = TypeVar("T")
 
 
 def _json_encoder(obj: Any) -> Any:  # noqa: PLR0911
-    """Convert objects to a form suitable for flatterization."""
+    """Convert objects to a form suitable for flattening."""
     if isinstance(obj, (set, tuple)):
         return list(obj)
     if isinstance(obj, enum.Enum):
         return obj.value
-    if hasattr(obj, 'as_compressed_state'):
+    if hasattr(obj, "as_compressed_state"):
         return obj.as_compressed_state
-    if hasattr(obj, 'as_dict'):
+    if hasattr(obj, "as_dict"):
         return obj.as_dict()
     if isinstance(obj, Path):
         return obj.as_posix()
@@ -40,7 +43,7 @@ def _flatten(x: Any, name: str, flattened_json: dict, exclude: set[str] | None):
 
     if isinstance(x, dict):
         for a in x:
-            _flatten(x[a], f'{name}.{a}' if name else a, flattened_json, exclude)
+            _flatten(x[a], f"{name}.{a}" if name else a, flattened_json, exclude)
     elif isinstance(x, Iterable):
         if not isinstance(x, (str, bytes, bytearray)):
             flattened_json[name] = str([_json_encoder(i) for i in x])
@@ -77,7 +80,7 @@ def flatten_json(json: Mapping, exclude: set[str] | None = None) -> dict:
                     },
                     'arr': [1, 2, 3],
                 },
-                'blip': 'blop',
+                'blip': 'blip',
             },
             exclude={'a.unwanted'},
         )
@@ -85,9 +88,22 @@ def flatten_json(json: Mapping, exclude: set[str] | None = None) -> dict:
         {
             'a.b.c': 'test',
             'a.arr': '[1, 2, 3]',
-            'blip': 'blop'
+            'blip': 'blip'
         }
     """
     flattened_json = {}
-    _flatten(json, '', flattened_json, exclude)
+    _flatten(json, "", flattened_json, exclude)
     return flattened_json
+
+
+def chunks(iterable: Iterable[T], size: int) -> Iterator["itertools.chain[T]"]:
+    """
+    Iterate over iterable in chunks.
+
+    Args:
+        iterable: an iterable to iterate over
+        size: single chunk size
+    """
+    iterator = iter(iterable)
+    for first in iterator:
+        yield itertools.chain([first], itertools.islice(iterator, size - 1))
